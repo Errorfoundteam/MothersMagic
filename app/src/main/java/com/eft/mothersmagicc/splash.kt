269002adcac139581bfec74fae9781a66a8306b3
aryan2121 <kayoukaan@gmail.com>
2021-04-22 13:41:47 -0700

    splas.kt

--- app/src/main/java/com/eft/mothersmagicc/splash.kt
+++ app/src/main/java/com/eft/mothersmagicc/splash.kt
@@ -1,11 +1,15 @@
 package com.eft.mothersmagicc
 
+import android.app.ActivityOptions
 import android.content.Intent
+import android.media.Image
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.view.animation.Animation
 import android.view.animation.AnimationUtils
+import android.widget.ImageView
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.firebase.auth.FirebaseAuth
 
@@ -13,8 +17,9 @@ class splash : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
-//        val topamin : Animation = AnimationUtils.loadAnimation(this, top_animation)
-//        logo.animation = topamin
+        val logo=findViewById<ImageView>(R.id.logoo)
+        val topamin : Animation = AnimationUtils.loadAnimation(this,R.anim.top_animation)
+        logo.animation = topamin
 
         val phAccount = FirebaseAuth.getInstance()
         val googleAccount = GoogleSignIn.getLastSignedInAccount(this)
@@ -23,18 +28,22 @@ class splash : AppCompatActivity() {
         val handler = Handler()
         handler.postDelayed(
                 {
-//                    val intent = Intent(this,LoginActivity::class.java)
+                    val intent = Intent(this,MainActivity::class.java)
                     when {
                         phAccount.currentUser != null -> {
-//                            startActivity(Intent(this, BottomNavigationBar::class.java))
+                            startActivity(Intent(this, MainActivity::class.java))
 
                         }
                         googleAccount != null -> {
-//                            startActivity(Intent(this, MapActivity::class.java))
+                            startActivity(Intent(this, MainActivity::class.java))
                         }
                         else -> {
-//                        val options = ActivityOptions.makeSceneTransitionAnimation(this,
-//                        logo,"logoAnimationToLogin")
+                        val options = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                            ActivityOptions.makeSceneTransitionAnimation(this,
+                            logo,"logoAnimationToLogin")
+                        } else {
+                            TODO("VERSION.SDK_INT < LOLLIPOP")
+                        }
                             startActivity(intent)
                         }
                     }
