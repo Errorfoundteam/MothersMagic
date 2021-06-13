c3777082070cd1db6f47aac25515cf2f1e4eb4a4
aryan2121 <kayoukaan@gmail.com>
2021-06-13 18:14:16 +0530

    home base

---
+++ app/src/main/java/com/eft/mothersmagicc/model/savedata.java
@@ -0,0 +1,10 @@
+package com.eft.mothersmagicc.model;
+
+import kotlin.text.UStringsKt;
+
+public class savedata {
+    public static String location="no";
+    public savedata(String loc){
+        location=loc;
+    }
+}
