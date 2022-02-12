5992aea088f537f8c2a14dfd12a2dcc4349ac976
Sourav Kumar <sourav.kumar54325@gmail.com>
2022-02-13 01:50:58 +0530

    Sourav test

---
+++ app/src/main/java/com/eft/mothersmagicc/SouravTest.java
@@ -0,0 +1,14 @@
+package com.eft.mothersmagicc;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SouravTest extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sourav_test);
+    }
+}
