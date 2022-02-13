dbd145ae5e4768b0b186de7c64bce3ff1efa996e
aryan2121 <kayoukaan@gmail.com>
2022-02-13 07:04:48 +0530

    HomePage2.0 13-02-22-Aman

--- app/src/main/java/com/eft/mothersmagicc/Homepage.java
+++ app/src/main/java/com/eft/mothersmagicc/Homepage.java
@@ -4,32 +4,47 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.ScrollView;
 import android.widget.Spinner;
 import android.widget.Toast;
 
 import com.eft.mothersmagicc.Adapter.HomeRecyclerViewAdapter;
+import com.eft.mothersmagicc.Adapter.ViewPagerAdapter;
 import com.etebarian.meowbottomnavigation.MeowBottomNavigation;
+import com.synnapps.carouselview.CarouselView;
+import com.synnapps.carouselview.ImageListener;
+import com.tbuonomo.viewpagerdotsindicator.SpringDotsIndicator;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 import kotlin.Unit;
 import kotlin.jvm.functions.Function1;
 
 public class Homepage extends AppCompatActivity {
-RecyclerView HomeRecyclerView;
-ConstraintLayout SearchLayout,ProfilepageLayout;
-LinearLayout HompageLayout;
+
+    CarouselView carouselView;
+ConstraintLayout ProfilepageLayout;
+    int[] sampleImages = {R.drawable.testadd, R.drawable.testadd, R.drawable.testadd, R.drawable.testadd, R.drawable.testadd};
+
+    private ViewPagerAdapter homeslideAdapter;
+ScrollView HompageLayout;
+    int currentpage=-1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_homepage);
         final MeowBottomNavigation bottomNavigation=findViewById(R.id.bottomNavigation);
-        HomeRecyclerView=findViewById(R.id.HomeRecycleId);
+
         HompageLayout=findViewById(R.id.hopagelayout);
-        SearchLayout=findViewById(R.id.searchlayout);
         ProfilepageLayout=findViewById(R.id.profilepagelayout);
 
         Spinner location_spinner=findViewById(R.id.SpinnerAreaId);
@@ -38,33 +53,19 @@ LinearLayout HompageLayout;
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_item);
         location_spinner.setAdapter(adapter);
 //        s7=getResources().getStringArray(R.array.name);
-        HomeRecyclerViewAdapter myadapter= new HomeRecyclerViewAdapter(this);
-        HomeRecyclerView.setAdapter(myadapter);
-        HomeRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+
+
+        carouselView = findViewById(R.id.AddCarouselHomepage);
+        carouselView.setPageCount(sampleImages.length);
+        carouselView.setImageListener(imageListener);
 
 
 
 
-//        homerecycl.addOnScrollListener(new RecyclerView.OnScrollListener() {
-//            @Override
-//            public void onScrollStateChanged(@NonNull RecyclerView recyclerView, int newState) {
-//                super.onScrollStateChanged(recyclerView, newState);
-//            }
-//
-//            @Override
-//            public void onScrolled(@NonNull RecyclerView recyclerView, int dx, int dy) {
-//                if (dy > 0 && bottomNavigation.isShown()) {
-//                    H_notification_bar.setVisibility(View.GONE);
-//                    bottomNavigation.setVisibility(View.GONE);
-//                } else if (dy < 0) {
-//                    H_notification_bar.setVisibility(View.VISIBLE);
-//                    bottomNavigation.setVisibility(View.VISIBLE);
-//                }
-//            }
-//        });
+
 
         bottomNavigation.show(1,true);
-        bottomNavigation.add(new MeowBottomNavigation.Model(0,R.drawable.ic_search));
         bottomNavigation.add(new MeowBottomNavigation.Model(1,R.drawable.ic_home));
         bottomNavigation.add(new MeowBottomNavigation.Model(2,R.drawable.ic_user));
 
@@ -73,23 +74,15 @@ LinearLayout HompageLayout;
             public Unit invoke(MeowBottomNavigation.Model model) {
                 switch (model.getId()){
                     case 0:
-                        SearchLayout.setVisibility(View.VISIBLE);
-                        HompageLayout.setVisibility(View.GONE);
-                        ProfilepageLayout.setVisibility(View.GONE);
-                        Toast.makeText(Homepage.this, "one", Toast.LENGTH_SHORT).show();
-//                        openlayout("search",1);
-                        break;
 
-                    case 1:
-                        SearchLayout.setVisibility(View.GONE);
                         HompageLayout.setVisibility(View.VISIBLE);
                         ProfilepageLayout.setVisibility(View.GONE);
                         Toast.makeText(Homepage.this, "two", Toast.LENGTH_SHORT).show();
 //                        openlayout("home", 2);
                         break;
 
-                    case 2:
-                        SearchLayout.setVisibility(View.GONE);
+                    case 1:
+
                         HompageLayout.setVisibility(View.GONE);
                         ProfilepageLayout.setVisibility(View.VISIBLE);
                         Toast.makeText(Homepage.this, "three", Toast.LENGTH_SHORT).show();
@@ -97,7 +90,6 @@ LinearLayout HompageLayout;
                         break;
                     default:
 
-                        SearchLayout.setVisibility(View.GONE);
                         HompageLayout.setVisibility(View.VISIBLE);
                         ProfilepageLayout.setVisibility(View.GONE);
 //                        openlayout("home", 2);
@@ -111,4 +103,11 @@ LinearLayout HompageLayout;
 
     }
 
+
+    ImageListener imageListener = new ImageListener() {
+        @Override
+        public void setImageForPosition(int position, ImageView imageView) {
+            imageView.setImageResource(sampleImages[position]);
+        }
+    };
 }
