a90314f6a7156fa7737279f6c327ead069ead35e
aryan2121 <kayoukaan@gmail.com>
2022-02-14 04:27:36 +0530

    HomePage3.0 14-02-22-Aman

--- app/src/main/java/com/eft/mothersmagicc/Adapter/HomeRecyclerViewAdapter.java
+++ app/src/main/java/com/eft/mothersmagicc/Adapter/HomeRecyclerViewAdapter.java
@@ -1,13 +1,19 @@
 package com.eft.mothersmagicc.Adapter;
 
 import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 
 import androidx.annotation.NonNull;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.eft.mothersmagicc.MainActivity;
+import com.eft.mothersmagicc.OpenActivity;
 import com.eft.mothersmagicc.R;
 
 import java.util.zip.Inflater;
@@ -15,10 +21,12 @@ import java.util.zip.Inflater;
 public class HomeRecyclerViewAdapter  extends RecyclerView.Adapter<HomeRecyclerViewAdapter.MyViewHolder> {
     @NonNull
     Context context;
+    String ImageArray[];
 
-
-    public  HomeRecyclerViewAdapter(Context ct){
+    public  HomeRecyclerViewAdapter(Context ct)
+    {
         context=ct;
+//        ImageArray=imagearray;
     }
     @Override
     public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
@@ -29,6 +37,16 @@ public class HomeRecyclerViewAdapter  extends RecyclerView.Adapter<HomeRecyclerV
 
     @Override
     public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+        Animation animatn= AnimationUtils.loadAnimation(context,R.anim.zoomin);
+        holder.baselayout.startAnimation(animatn);
+        holder.baselayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i=new Intent(context, OpenActivity.class);
+
+                context.startActivity(i);
+            }
+        });
 
     }
 
@@ -38,8 +56,11 @@ public class HomeRecyclerViewAdapter  extends RecyclerView.Adapter<HomeRecyclerV
     }
 
     public class MyViewHolder extends RecyclerView.ViewHolder {
+        ConstraintLayout baselayout;
         public MyViewHolder(@NonNull View itemView) {
+
             super(itemView);
+            baselayout=itemView.findViewById(R.id.itembaselayout);
         }
     }
 }
